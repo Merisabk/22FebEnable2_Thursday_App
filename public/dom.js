cbfa41ba82de69c6c769fc8f7a6522cdbbc8a7bf
Merisa <bishwakarma.merisa@gmail.com>
2022-04-11 20:30:24 +0100

    worked on readbyId method

--- public/dom.js
+++ public/dom.js
@@ -4,3 +4,13 @@ export const inputPrice = document.getElementById(`inputPrice`);
 export const buttonCreate = document.getElementById(`buttonCreate`);
 
 export const listOutput = document.getElementById(`listOutput`);
+export const updateName = document.getElementById(`updateName`);
+export const updateDescription = document.getElementById(`updateDescription`);
+export const updatePrice = document.getElementById(`updatePrice`);
+export const buttonUpdate = document.getElementById(`buttonUpdate`);
+export const buttonDelete = document.getElementById(`buttonDelete`);
+export const deleteInput = document.getElementById(`deleteInput`);
+export const idInput = document.getElementById(`idInput`);
+export const readByIdOutput = document.getElementById(`readByIdOutput`);
+export const readByIdButton = document.getElementById(`readByIdButton`);
+export const readIdInput = document.getElementById(`itemId`);
--- public/script.js
+++ public/script.js
@@ -11,6 +11,13 @@ const writeItem = item => {
   DOM.listOutput.appendChild(child);
 }
 
+const writeItemId = item => {
+  const child = document.createElement(`li`);
+  child.id = item._id;
+  child.innerHTML = `${JSON.stringify(item)}`;
+  DOM.readByIdOutput.appendChild(child);
+}
+
 // GET all function
 const get = () => {
   DOM.listOutput.innerHTML = ``;
@@ -29,6 +36,25 @@ const get = () => {
     });
 }
 
+// GET by ID function
+
+const readById = () => {
+  DOM.readByIdOutput.innerHTML=``;
+
+  axios.get(`/read/${itemId.value}`)
+  .then((response) => {
+    if (!Array.isArray(response.data)) {
+      writeItemId(response.data);
+    } else {
+      for (let item of response.data) {
+        writeItemId(item);
+  }
+}
+}).catch((err) => {
+  console.log(err);
+});
+}
+
 // POST function
 const post = () => {
   axios.post(`/create`, {   name : DOM.inputName.value,
@@ -42,8 +68,38 @@ const post = () => {
     });
 }
 
+// PUT function 
+const put = () => {
+  axios.put(`/update/${idInput.value}`, {
+                          name : DOM.updateName.value,
+                          description : DOM.updateDescription.value, 
+                          price : DOM.updatePrice.value })
+    .then((response) => {
+      console.log(response);
+      get();
+    }).catch((err) => {
+      console.log(err, (err.response));
+    });
+
+  }
+
+  // DELETE function
+
+  const deletePost = () => {
+    axios.delete(`/delete/${deleteInput.value}`)
+    .then ((response) => {
+      console.log(response);
+      get();
+    }).catch((err) => {
+      console.log(err);
+    })
+      }
+    
 // set up the buttons' on click events
 DOM.buttonCreate.onclick = () => post();
+DOM.buttonUpdate.onclick = () => put();
+DOM.buttonDelete.onclick = () => deletePost();
+DOM.readByIdButton.onclick = () => readById();
 
 // run the get function on page load
 get();
